0394fa37c510f7783bd6d5d2807e4d8763bbe3d4
Jakob <88966745+kometenmelodie@users.noreply.github.com>
2023-08-22 16:18:50 +0200

    feat: ✨ add Pokémon pictures as nodes

--- pokenet/core.py
+++ pokenet/core.py
@@ -9,30 +9,49 @@ class PokeNet:
         self.node_data = pl.read_csv(node_data_path)
         self.net = None
 
-    def create_network(self, generation: int | None = 2):
+    def create_network(
+        self, pokemon_generation: int | None = 2, pokemon_pics_path: str | None = None
+    ):
         # initialize network
         net = Network(
             height="900px", bgcolor="black", font_color="white", filter_menu=False
         )
-        # filter self.node_data by generation
-        if generation is not None:
-            net_data = self.node_data.filter(pl.col("generation_id") == generation)
+        # filter self.node_data by Pokémon generation
+        if pokemon_generation is not None:
+            net_data = self.node_data.filter(
+                pl.col("generation_id") == pokemon_generation
+            )
         else:
             net_data = self.node_data
 
         for pokemon in net_data.rows(named=True):
-            # add source node
-            net.add_node(
-                pokemon["source_node"], title=pokemon["source_node"], color="white"
-            )
-            # add target node
-            net.add_node(
-                pokemon["target_node"], title=pokemon["target_node"], color="white"
-            )
+            source_node = pokemon["source_node"]
+            target_node = pokemon["target_node"]
+
+            if pokemon_pics_path is None:
+                # add source node
+                net.add_node(source_node, title=source_node, color="white")
+                # add target node
+                net.add_node(target_node, title=target_node, color="white")
+            else:
+                source_id = pokemon["source_id"]
+                target_id = pokemon["target_id"]
+
+                # add pictures of Pokémon as nodes
+                net.add_node(
+                    source_node,
+                    title=source_node,
+                    shape="image",
+                    image=f"{pokemon_pics_path}/{source_id}.png",
+                )
+                net.add_node(
+                    target_node,
+                    title=target_node,
+                    shape="image",
+                    image=f"{pokemon_pics_path}/{target_id}.png",
+                )
             # add edge
-            net.add_edge(
-                pokemon["source_node"], pokemon["target_node"], value=1.5, color="white"
-            )
+            net.add_edge(source_node, target_node, value=1.5, color="white")
 
         self.net = net
 
